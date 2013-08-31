bac9249b0fa76d110936e214a7c008c5533df8a7
nilanjanray <nilanjan.ray@ericsson.com>
2013-08-31 19:19:10 +0200

    Vhosts & symlinks added by puppet

--- manifests/classes/custom_apache_php.pp
+++ manifests/classes/custom_apache_php.pp
@@ -3,9 +3,61 @@ class custom_apache_php {
   #include apache
   class { 'apache':
     mpm_module => 'prefork',
+    default_vhost => false,
   }
   
   include apache::mod::rewrite
+  # If need to open any extra port we can do it from below syntax.
+  # apache::listen { '8081': }  
+
+  $dir = 'app/drupalroot/web'
+  #$drupaldir = 'www'
+
+  exec { 'mkdir_cert_dir':
+    command => "mkdir -p /opt/${dir}",
+    unless  => "test -d /opt/${dir}",
+  }
+
+  file { "/srv/www":
+    ensure => "directory",
+  }
+
+  # Ensure to create soft-link.
+  file { '/srv/www/drupal':
+    ensure => 'link',
+    target => '/opt/app/drupalroot',
+  }
+  
+  # create log directory.
+  file { "/srv/www/drupal/log":
+    ensure => "directory",
+  }
+
+  # create error log.
+  file { "/srv/www/drupal/log/drupal_error.log":
+    ensure => "present",
+  }
+
+
+  $docroot = '/srv/www/drupal/web'
+  $error_log_file = '/srv/www/drupal/log/drupal_error.log'
+  $scriptalias          = '/usr/lib/cgi-bin'
+  $access_log_file      = '/srv/www/drupal/access-log/access_drual.log'
+
+  apache::vhost { 'drupal':
+    port => 8081,
+    docroot => $docroot,
+    scriptalias => $scriptalias,
+    serveradmin => $serveradmin,
+    override => "All",
+    error_log => true,
+    access_log => true,
+    #error_log_file => $error_log_file,
+    #access_log_file => $access_log_file,
+    #priority => '15',
+  }
+
+
 
   class {'apache::mod::php':
     require => Package["php5"]
