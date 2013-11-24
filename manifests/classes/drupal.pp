8a1baf4631c7b9c54fba0b355b3c85fb9bcfc9a4
nilanjanray <nilanjan.ray@ericsson.com>
2013-11-24 13:37:42 +0530

    Permission problem fixed using chown over files directory drupal

--- manifests/classes/drupal.pp
+++ manifests/classes/drupal.pp
@@ -23,9 +23,23 @@ class drupal {
     #user => 'root'
   #}
 
+  #exec { "rm -rf /srv/www/drupal/web":
+   # command => "rm -rf /srv/www/drupal/web",
+    #user => 'root',
+  #}
+
+
   #Execution & Installation of drush.
   drush::exec { 'drush-drupal-download':
-    command        => 'dl drupal --destination=/srv/www/drupal',
+    command        => 'dl --drupal-project-rename=drop1 drupal --destination=/srv/www/drupal/web',
     #root_directory => '/srv/www/drupal',
+  }
+  
+  #Change Owner ship to apache
+  file { "/srv/www/drupal/web/drop1/sites/default" : 
+    ensure => directory,
+    group => "www-data",
+    owner => "www-data",
+    recurse => true,     
   }
 }
